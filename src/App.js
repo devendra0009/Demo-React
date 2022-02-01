0ddb1fe1f67b9c8d83bc674137acd359727c3383
Davendra Singh Bedwal <devendrabedwal4@gmail.com>
2022-02-02 01:07:44 +0530

    update

--- src/App.js
+++ src/App.js
@@ -3,22 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>
+        Lorem Ipsum Doler
+      </h1>
+      <div className="lorem">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil nesciunt fugiat, distinctio quod esse perspiciatis, quaerat aliquam excepturi asperiores quos eius. Labore, placeat tenetur quam, explicabo magni minus ipsam voluptas hic esse dicta neque natus ab repellendus, itaque similique. Similique accusantium eius expedita illo dolorem quis reiciendis inventore quidem, quos et assumenda quibusdam ab voluptatum dolor illum harum ipsam nihil tempora cum deserunt quam beatae ipsa. Cum adipisci eius eaque impedit fugit vel? Est consequatur voluptatibus, molestiae repellendus ullam nisi. Excepturi, officia distinctio eos, laudantium corrupti nostrum repellendus dolor beatae iure quibusdam libero doloribus culpa vitae, soluta necessitatibus tenetur nesciunt illum. Dolore libero quos expedita asperiores et quia odio molestias corporis dolorum facere architecto reprehenderit, praesentium officia totam earum nemo officiis aliquid laborum quas sit. Rem, repellendus quisquam saepe velit enim cum porro officia nemo, dolor eum harum obcaecati quia expedita temporibus doloremque sapiente, consectetur dolorum ex veritatis minus aperiam? Expedita odio consequuntur quidem earum veritatis saepe quisquam repudiandae minus nobis vel, debitis voluptas porro sequi minima doloribus cumque ex distinctio iste. Explicabo fugiat molestias perspiciatis quaerat ipsa architecto similique incidunt, eligendi illum sequi tenetur obcaecati commodi iste minus minima praesentium velit autem quod? Tempore libero debitis maxime aperiam non!</div>
+    </>
   );
 }
 
